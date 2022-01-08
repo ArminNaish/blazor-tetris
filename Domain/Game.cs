e32d3b8f0e7e7f3a10a320d209af06d6eadad47d
ArminNaish <armin.naish@outlook.com>
2022-01-08 12:48:38 +0100

    move game to domain

--- Domain/Game.cs
+++ Domain/Game.cs
@@ -1,5 +1,161 @@
 namespace BlazorTetris.Domain;
 
+// todo: extract classes
+
+public static class Extensions
+{
+    public static Task LoopAsync(this Action action, int timeout, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return Task.Run(async () =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(timeout, cancellationToken);
+                    action();
+                }
+            }, cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            // Task cancelled
+            return Task.CompletedTask;
+        }
+    }
+}
+
+public sealed class Game : IDisposable
+{
+    public delegate void StateChangedEventHandler();
+    public event StateChangedEventHandler? OnStateChanged;
+    
+    private CancellationTokenSource _gameLoopCancellation = new ();
+    private List<Tetromino> _tetrominos = new();
+    private Tetromino _currentTetromino = null!;
+    private int _width;
+    private int _currentPosition;
+    private int _currentRotation;
+    private bool _isInitialized;
+
+    public List<Square> Squares { get; } = new();
+
+    public void Initialize()
+    {
+        _width = 10;
+        _currentPosition = 4;
+        _currentRotation = 0;
+        
+        _tetrominos = new List<Tetromino>
+        {
+            Tetromino.L(_width), 
+            Tetromino.Z(_width), 
+            Tetromino.T(_width), 
+            Tetromino.O(_width), 
+            Tetromino.I(_width)
+        };
+        
+        var squares = Enumerable.Range(0, 200)
+            .Select(index => new Square{ Index = index})
+            .ToList();
+
+        var taken = Enumerable.Range(200, 10)
+            .Select(index => new Square{ Index = index, IsTaken = true})
+            .ToList();
+        
+        Squares.AddRange(squares);
+        Squares.AddRange(taken);
+        
+        _currentTetromino = RandomTetromino();
+
+        _isInitialized = true;
+    }
+
+    public void Draw()
+    {
+        if (!_isInitialized) return;
+        
+        DrawTetromino();
+
+        _ =  new Action(MoveDownTetromino).LoopAsync(1000, _gameLoopCancellation.Token);
+    }
+  
+    private void MoveDownTetromino()
+    {
+        UndrawTetromino();
+        _currentPosition += _width;
+        DrawTetromino();
+        
+        // collision detection: check if tetromino at current position hits a taken square ahead
+        var anyTakenSquaresAhead = _currentTetromino
+            .GetIndizes()
+            .Select(index => Squares[_currentPosition + index + _width])
+            .Any(square => square.IsTaken);
+
+        // todo: extract methods
+        
+        if (!anyTakenSquaresAhead) return;
+        
+        // mark squares of current tetromino as taken
+        _currentTetromino
+            .GetIndizes()
+            .Select(index => Squares[_currentPosition + index])
+            .ToList()
+            .ForEach(square => square.IsTaken = true);
+        
+        // start a new tetromino falling
+        _currentTetromino = RandomTetromino();
+        _currentPosition = 4;
+        DrawTetromino();
+    }
+    
+    private void DrawTetromino()
+    {
+        _currentTetromino
+            .GetIndizes()
+            .Select(index => Squares[_currentPosition + index])
+            .ToList()
+            .ForEach(square => square.IsTetromino = true);
+        
+        OnStateChanged?.Invoke();
+    }
+
+    private void UndrawTetromino()
+    {
+        _currentTetromino
+            .GetIndizes()
+            .Select(index => Squares[_currentPosition + index])
+            .ToList()
+            .ForEach(square => square.IsTetromino = false);
+        
+        OnStateChanged?.Invoke();
+    }
+    
+    private Tetromino RandomTetromino()
+    {
+        var random = new Random();
+        return _tetrominos[random.Next(0, _tetrominos.Count)];
+    }
+    
+    public void Dispose()
+    {
+        try
+        {
+            _gameLoopCancellation.Cancel();
+        }
+        catch (Exception)
+        {
+        }
+    }
+}
+
+public class Square
+{
+    public int Index { get; set; }
+    public bool IsTetromino { get; set; }
+    public bool IsTaken { get; set; }
+}
+
 public record Rotation(int I1, int I2, int I3, int I4);
 
 public enum RotationType { Up, Right, Down, Left }
